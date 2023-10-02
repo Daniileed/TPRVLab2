5ca3d77e726e86b7e8f42c8ab456c1096b2a4bf4
Daniileed <daniil.orlov@mail.ru>
2023-10-03 06:48:18 +0700

    Программа доделана

--- TPRVLab2Task1/TPRVLab2Task1.cpp
+++ TPRVLab2Task1/TPRVLab2Task1.cpp
@@ -10,43 +10,41 @@
 using namespace cv;
 using namespace std;
 
-const int MAXTHREADS = 4;
+const int Length =2048;
+const int Height = 1536;
+string filename = "C://Users//danii//source//repos//TPRVLab2Task1//" + std::to_string(Length) + "x" + std::to_string(Height) + ".jpg";
 
-const int Length = 1024; 
-const int Height = 768; 
-string filename = "C://Users//danii//source//repos//TPRVLab2Task1//" + std::to_string(Length) + "x"+ std::to_string(Height) +".jpg";
-
-int**  Rv = new int* [Height];
-int**  Gv = new int*[Height];
-int**  Bv = new int* [Height];
+int** Rv = new int* [Height];
+int** Gv = new int* [Height];
+int** Bv = new int* [Height];
 int** Iv = new int* [Height];
 
 int** MRv = new int* [Height];
 int** MX = new int* [Height];
 int** MY = new int* [Height];
 
-int MRMax = 0;
+int MRMax = 1;
 
-const int FSX[3][3] = { 
-{ -1,0,1 },
-{-2,0,2},
-{-1,0,1 } 
+const int FSX[3][3] = {
+{ -1,0,1 },  
+    {-2,0,2},
+{-1,0,1 }
 };
 
-const int FSY[3][3] = { 
+const int FSY[3][3] = {
     {-1,-2,-1},
     {0,0,0},
-    {1,2,1 }};
+    {1,2,1 } };
 
 
-void check_elementX(int i, int j, int elX, int elY) {
-    if ((i>= 0) && (i< Height) && (j>=0) && (j< Length))
-    MX[i][j] = Iv[i][j] + FSX[elX][elY];
+void check_elementX(int curI, int curJ,int i, int j, int elX, int elY) {
+    if ((i >= 0) && (i < Height) && (j >= 0) && (j < Length))
+        MX[curI][curJ]+= Iv[i][j]*FSX[elX][elY];
 }
 
-void check_elementY(int i, int j, int elX, int elY) {
+void check_elementY(int curI, int curJ, int i, int j, int elX, int elY) {
     if ((i >= 0) && (i < Height) && (j >= 0) && (j < Length))
-        MY[i][j] = Iv[i][j] + FSY[elX][elY];
+        MY[curI][curJ]+= Iv[i][j]*FSY[elX][elY];
 }
 
 int main() {
@@ -55,19 +53,23 @@ int main() {
         Gv[i] = new int[Length];
         Bv[i] = new int[Length];
 
-        Iv[i] = new int [Length];
+        Iv[i] = new int[Length];
 
-        MRv[i] = new int [Length];
-        MX[i] = new int [Length];
-        MY[i] = new int [Length];
+        MRv[i] = new int[Length];
+        MX[i] = new int[Length];
+        MY[i] = new int[Length];
     }
 
     Mat img_color = imread(filename, 1);
-    namedWindow("color image", WINDOW_NORMAL);
-    imshow("color image", img_color);
-    waitKey(0);
-    destroyAllWindows();
-    
+
+    for (int i = 0; i < Height; i++) {
+        for (int j = 0; j < Length; j++) {
+            MX[i][j] = 0;
+            MY[i][j] = 0;
+            MRv[i][j] = 0;
+        }
+    }
+
     //Single thread
     auto start_1 = std::chrono::high_resolution_clock::now();
 
@@ -83,32 +85,32 @@ int main() {
 
     for (int i = 0; i < Height; i++) {
         for (int j = 0; j < Length; j++) {
-            check_elementX(i - 1, j - 1, 0,0);
-            check_elementX(i , j - 1, 1, 0);
-            check_elementX(i + 1, j - 1, 2, 0);
+            check_elementX(i,j,i - 1, j - 1, 0, 0);
+            check_elementX(i, j, i, j - 1, 1, 0);
+            check_elementX(i, j, i + 1, j - 1, 2, 0);
 
-            check_elementX(i - 1, j + 1, 0, 2);
-            check_elementX(i , j + 1, 1, 2);
-            check_elementX(i + 1, j + 1, 2, 2);
+            check_elementX(i, j, i - 1, j + 1, 0, 2);
+            check_elementX(i, j, i, j + 1, 1, 2);
+            check_elementX(i, j, i + 1, j + 1, 2, 2);
 
-                }
-            }
-
-    for (int i = 0; i < Height; i++) {
-        for (int j = 0; j < Length; j++) {
-           check_elementY(i - 1, j - 1, 0, 0);
-            check_elementY(i - 1, j , 0, 1);
-            check_elementY(i - 1, j + 1, 0, 2);
-            
-            check_elementY(i + 1, j - 1, 2, 0);
-            check_elementY(i + 1, j, 2, 1);
-            check_elementY(i + 1, j + 1, 2, 2);
         }
     }
 
     for (int i = 0; i < Height; i++) {
         for (int j = 0; j < Length; j++) {
-           MRv[i][j] = floor(sqrt(pow(MX[i][j], 2) + pow(MY[i][j], 2)));
+            check_elementY(i,j, i - 1, j - 1, 0, 0);
+            check_elementY(i,j, i - 1, j, 0, 1);
+            check_elementY(i,j, i - 1, j + 1, 0, 2);
+
+            check_elementY(i, j, i + 1, j - 1, 2, 0);
+            check_elementY(i, j, i + 1, j, 2, 1);
+            check_elementY(i, j, i + 1, j + 1, 2, 2);
+        }
+    }
+
+    for (int i = 0; i < Height; i++) {
+        for (int j = 0; j < Length; j++) {
+            MRv[i][j] = floor(sqrt(pow(MX[i][j], 2) + pow(MY[i][j], 2)));
         }
     }
 
@@ -118,7 +120,7 @@ int main() {
                 MRMax = MRv[i][j];
         }
     }
- 
+
     for (int i = 0; i < Height; i++) {
         for (int j = 0; j < Length; j++) {
             MRv[i][j] = (MRv[i][j] * 255) / MRMax;
@@ -133,23 +135,119 @@ int main() {
         }
     }
 
-    namedWindow("Sobel_scale", WINDOW_NORMAL);
-    imshow("Sobel_scale", Sobel_scale);
-    waitKey(0);
-    destroyAllWindows();
-   
-
+    
 
     auto end_1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> dur1 = (end_1 - start_1) * 1000;
     std::cout << "Time: " << dur1.count() << " milliseconds\n\n";
 
+    namedWindow("Sobel_scale", WINDOW_NORMAL);
+    imshow("Sobel_scale", Sobel_scale);
+    waitKey(0);
+    destroyAllWindows();
+
+    for (int i = 0; i < Height; i++) {
+        for (int j = 0; j < Length; j++) {
+            Iv[i][j] = 0;
+            MX[i][j] = 0;
+            MY[i][j] = 0;
+            MRv[i][j] = 0;
+        }
+    }
+
     //OpenMP
     auto start_2 = std::chrono::high_resolution_clock::now();
+
+#pragma omp parallel 
+    {
+#pragma omp for 
+        for (int i = 0; i < Height; i++) {
+            for (int j = 0; j < Length; j++) {
+                Bv[i][j] = img_color.at<cv::Vec3b>(i, j)[0];
+                Gv[i][j] = img_color.at<cv::Vec3b>(i, j)[1];
+                Rv[i][j] = img_color.at<cv::Vec3b>(i, j)[2];
+
+                Iv[i][j] = floor((Rv[i][j] + Gv[i][j] + Bv[i][j]) / 3);
+            }
+        }
+    
+
+#pragma omp sections nowait
+    {
+#pragma omp section 
+        {
+            for (int i = 0; i < Height; i++) {
+                for (int j = 0; j < Length; j++) {
+                    MX[i][j] = Iv[i][j];
+                    check_elementX(i, j, i - 1, j - 1, 0, 0);
+                    check_elementX(i, j, i, j - 1, 1, 0);
+                    check_elementX(i, j, i + 1, j - 1, 2, 0);
+
+                    check_elementX(i, j, i - 1, j + 1, 0, 2);
+                    check_elementX(i, j, i, j + 1, 1, 2);
+                    check_elementX(i, j, i + 1, j + 1, 2, 2);
+
+                }
+            }
+        }
+#pragma omp section 
+        {
+            for (int i = 0; i < Height; i++) {
+                for (int j = 0; j < Length; j++) {
+                    MY[i][j] = Iv[i][j];
+                    check_elementY(i, j, i - 1, j - 1, 0, 0);
+                    check_elementY(i, j, i - 1, j, 0, 1);
+                    check_elementY(i, j, i - 1, j + 1, 0, 2);
+
+                    check_elementY(i, j, i + 1, j - 1, 2, 0);
+                    check_elementY(i, j, i + 1, j, 2, 1);
+                    check_elementY(i, j, i + 1, j + 1, 2, 2);
+                }
+            }
+        }
+    }
+#pragma omp for 
+    for (int i = 0; i < Height; i++) {
+        for (int j = 0; j < Length; j++) {
+            MRv[i][j] = floor(sqrt(pow(MX[i][j], 2) + pow(MY[i][j], 2)));
+        }
+    }
+#pragma omp for 
+    for (int i = 0; i < Height; i++) {
+        for (int j = 0; j < Length; j++) {
+            if (MRv[i][j] >= MRMax)
+                MRMax = MRv[i][j];
+        }
+    }
+#pragma omp for 
+    for (int i = 0; i < Height; i++) {
+        for (int j = 0; j < Length; j++) {
+            MRv[i][j] = (MRv[i][j] * 255) / MRMax;
+        }
+    }
+
+}
+
+    Mat Sobel_scale_OMP = Mat::zeros(Height, Length, CV_8UC1);
+
+#pragma omp parallel  for
+        for (int i = 0; i < Height; i++) {
+            for (int j = 0; j < Length; j++) {
+                Sobel_scale_OMP.at<uchar>(i, j) = MRv[i][j];
+            }
+        }
+
+        
+
 
     auto end_2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> dur2 = (end_2 - start_2) * 1000;
     std::cout << "Time: " << dur2.count() << " milliseconds\n\n";
+
+    namedWindow("Sobel_scale", WINDOW_NORMAL);
+    imshow("Sobel_scale", Sobel_scale);
+    waitKey(0);
+    destroyAllWindows();
 
     return 0;
 }
--- TPRVLab2Task2/TPRVLab2Task2.cpp
+++ TPRVLab2Task2/TPRVLab2Task2.cpp
@@ -5,53 +5,16 @@
 #include <string>
 #include <chrono>
 #include <immintrin.h>
+#include <omp.h>
 
-const int MAXTHREADS = 4;
 const int size = 4096;
-
 __int32** Am = new __int32* [size];
 __int32** Bm = new __int32* [size];
 __int32** Rs = new __int32* [size];
 __int32** Rv = new __int32* [size];
-__int32** ThreadS = new __int32* [size];
-__int32** ThreadV = new __int32* [size];
+__int32** OmpS = new __int32* [size];
+__int32** OmpV = new __int32* [size];
 
-void CalculateThread(int begin, int end) {
-
-    for (int i = begin; i < end; ++i) {
-        for (int j = 0; j < size; ++j) {
-            for (int k = 0; k < size; ++k) {
-                ThreadS[i][j] += Am[i][k] * Bm[k][j];
-            }
-        }
-    }
-}
-
-void CalculateVectorThread(int begin, int end) {
-
-    __int32** BT = new __int32* [size];
-    for (int i = 0; i < size; i++) {
-        BT[i] = new __int32[size];
-        for (int j = 0; j < size; j++) {
-            BT[i][j] = Bm[j][i];
-        }
-    }
-
-    for (int i = begin; i < end; i++) {
-        for (int j = 0; j < size; j++) {
-            __m128i sum = _mm_setzero_si128();
-            for (int k = 0; k < size; k += 4) {
-                __m128i al = _mm_loadu_si128((__m128i*) & Am[i][k]);
-                __m128i bl = _mm_loadu_si128((__m128i*) & BT[j][k]);
-                __m128i mult = _mm_mullo_epi16(al, bl);
-                sum = _mm_add_epi32(mult, sum);
-            }
-            __m128i line_sum = _mm_hadd_epi32(_mm_hadd_epi32(sum, sum), _mm_hadd_epi32(sum, sum));
-            _mm_storeu_si32((__m128i*) & ThreadV[i][j], line_sum);
-
-        }
-    }
-}
 
 int main()
 {
@@ -60,16 +23,16 @@ int main()
         Bm[i] = new __int32[size];
         Rs[i] = new __int32[size];
         Rv[i] = new __int32[size];
-        ThreadS[i] = new __int32[size];
-        ThreadV[i] = new __int32[size];
+        OmpS[i] = new __int32[size];
+        OmpV[i] = new __int32[size];
 
         for (int j = 0; j < size; j++) {
             Am[i][j] = rand() % 5;
             Bm[i][j] = rand() % 5;
             Rs[i][j] = 0;
             Rv[i][j] = 0;
-            ThreadS[i][j] = 0;
-            ThreadV[i][j] = 0;
+            OmpS[i][j] = 0;
+            OmpV[i][j] = 0;
         }
     }
 
@@ -95,6 +58,98 @@ int main()
             BT[i][j] = Bm[j][i];
         }
     }
+
+    std::cout << "Vector: \n";
+    auto start_2 = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            __m128i sum = _mm_setzero_si128();
+            for (int k = 0; k < size; k += 4) {
+                __m128i al = _mm_loadu_si128((__m128i*) & Am[i][k]);
+                __m128i bl = _mm_loadu_si128((__m128i*) & BT[j][k]);
+                __m128i mult = _mm_mullo_epi16(al, bl);
+                sum = _mm_add_epi32(mult, sum);
+            }
+            __m128i line_sum = _mm_hadd_epi32(_mm_hadd_epi32(sum, sum), _mm_hadd_epi32(sum, sum));
+            _mm_storeu_si32((__m128i*) & Rv[i][j], line_sum);
+
+        }
+    }
+
+    auto end_2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> dur2 = (end_2 - start_2) * 1000;
+    std::cout << "Time: " << dur2.count() << " milliseconds\n\n";
+
+    std::cout << "ScalarOMP: \n";
+    auto start_3 = std::chrono::high_resolution_clock::now();
+
+#pragma omp parallel  for
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                for (int k = 0; k < size; ++k) {
+                    OmpS[i][j] += Am[i][k] * Bm[k][j];
+                }
+            }
+        }
+    
+    auto end_3 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> dur3 = (end_3 - start_3) * 1000;
+    std::cout << "Time: " << dur3.count() << " milliseconds\n\n";
+
+    std::cout << "VectorOMP: \n";
+    auto start_4 = std::chrono::high_resolution_clock::now();
+
+#pragma omp parallel  for 
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                __m128i sum = _mm_setzero_si128();
+                for (int k = 0; k < size; k += 4) {
+                    __m128i al = _mm_loadu_si128((__m128i*) & Am[i][k]);
+                    __m128i bl = _mm_loadu_si128((__m128i*) & BT[j][k]);
+                    __m128i mult = _mm_mullo_epi16(al, bl);
+                    sum = _mm_add_epi32(mult, sum);
+                }
+                __m128i line_sum = _mm_hadd_epi32(_mm_hadd_epi32(sum, sum), _mm_hadd_epi32(sum, sum));
+                _mm_storeu_si32((__m128i*) & OmpV[i][j], line_sum);
+            }
+        }
+    
+
+    auto end_4 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> dur4 = (end_4 - start_4) * 1000;
+    std::cout << "Time: " << dur4.count() << " milliseconds\n\n";
+
+    bool equal = true;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if ((Rv[i][j] != Rs[i][j]) or (Rv[i][j] != OmpS[i][j]) or (Rv[i][j] != OmpV[i][j]))
+                equal = false;
+        }
+    }
+
+    std::string Isequal;
+    Isequal = equal ? "Matrix is equal\n" : "Matrix not equal\n";
+    std::cout << Isequal;
+
+
+    for (int i = 0; i < size; i++) {
+        delete[] Am[i];
+        delete[] Bm[i];
+        delete[] BT[i];
+        delete[] Rs[i];
+        delete[] Rv[i];
+        delete[] OmpS[i];
+        delete[] OmpV[i];
+    }
+
+    delete[] Am;
+    delete[] Bm;
+    delete[] BT;
+    delete[] Rs;
+    delete[] Rv;
+    delete[] OmpS;
+    delete[] OmpV;
 }
 
 
